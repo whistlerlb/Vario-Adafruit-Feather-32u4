6841433983db13ceacea5e8f28a8ad428a3dd395
Lionel Buzzetti <lionel.buzzetti@gmail.com>
2017-03-24 18:34:41 +0100

    GPS parser OK

--- variometer/variometer.ino
+++ variometer/variometer.ino
@@ -20,8 +20,8 @@
 #define HAVE_SPEAKER
 //#define HAVE_ACCELEROMETER
 #define HAVE_SCREEN
-//#define HAVE_GPS
-#define HAVE_BT
+#define HAVE_GPS
+//#define HAVE_BT
 
 
 // the variometer seems to be more stable at half speed
@@ -63,11 +63,11 @@ SSD1306_text oled(OLED_DC, OLED_RST, OLED_CS);
 /* alti/vario objects */
 /**********************/
 #define POSITION_MEASURE_STANDARD_DEVIATION 0.1
-#ifdef HAVE_ACCELEROMETER 
+#ifdef HAVE_ACCELEROMETER
 #define ACCELERATION_MEASURE_STANDARD_DEVIATION 0.3
 #else
 #define ACCELERATION_MEASURE_STANDARD_DEVIATION 0.6
-#endif //HAVE_ACCELEROMETER 
+#endif //HAVE_ACCELEROMETER
 
 kalmanvert kalmanvert;
 #ifdef HAVE_SPEAKER
@@ -124,9 +124,10 @@ float measuredvbat = 0;
 /*      SETUP      */
 /*-----------------*/
 void setup() {
-   
+
 Serial.begin(9600); //debug
-//while(!Serial);        //debug wait for the serial monitor.
+
+while(!Serial);        //debug wait for the serial monitor.
 //Serial.println("setup start");//debug
 
 /****************/
@@ -146,16 +147,16 @@ Serial1.begin(9600); //Serial1 is TX/RX on 32u4, Serial is for USB only
 #ifdef HAVE_SCREEN
 
 oled.init();
-oled.clear();  
+oled.clear();
 oled.setTextSize(2,2); // 2*(5x7) characters, pixel spacing = 2
 oled.setCursor(0,0); // setCursor(row, pixel)    - sets write location to the specified row (0 to 7) and pixel (0 to 127)
 oled.write("Alt: ");
 oled.setCursor(2,0); // setCursor(row, pixel)    - sets write location to the specified row (0 to 7) and pixel (0 to 127)
 oled.write("Alt0: ");
 
-  
+
 #endif //HAVE_SCREEN
-  
+
   /************************************/
   /* init altimeter and accelerometer */
   /************************************/
@@ -164,7 +165,7 @@ oled.write("Alt0: ");
   Fastwire::setup(400,0);
 #else
   Fastwire::setup(800,0);
-#endif //CPU_FREQ    
+#endif //CPU_FREQ
   ms5611_init();
 #ifdef HAVE_ACCELEROMETER
   vertaccel_init();
@@ -175,10 +176,9 @@ oled.write("Alt0: ");
   /************/
 #ifdef HAVE_GPS
   Serial1.begin(9600);
-  //Serial1.begin(38400);
 
 #endif //HAVE_GPS
-  
+
   /******************/
   /* get first data */
   /******************/
@@ -208,7 +208,7 @@ oled.write("Alt0: ");
                   millis());
 
 altTakeoff = ms5611_getAltitude(); //init altitude at takeoff  //TODO:do it better??
- 
+
 //Serial.println("Setup done"); //debug
 
 }
@@ -216,10 +216,10 @@ altTakeoff = ms5611_getAltitude(); //init altitude at takeoff  //TODO:do it bett
 /*----------------*/
 /*      LOOP      */
 /*----------------*/
-  
+
 
 void loop() {
- 
+
   /*****************************/
   /* compute vertical velocity */
   /*****************************/
@@ -232,7 +232,7 @@ void loop() {
                        vertaccel_getValue(),
                        millis() );
     NewDataEnable = 1; //for screen and BT update
-                       
+
 //    Serial.print("Alt:"); //DEBUG
 //    Serial.print(ms5611_getAltitude());//DEBUG
 //    Serial.print("  Vertaccel:"); //DEBUG
@@ -262,7 +262,7 @@ void loop() {
 
   /**************//**************//**************//**************//**************//**************//**************//**************/
   /**************/
-  /* Send BT info to XCSoar with OpenVario protocol */ 
+  /* Send BT info to XCSoar with OpenVario protocol */
   /**************/
 #ifdef HAVE_BT
 
@@ -271,14 +271,14 @@ void loop() {
   dtostrf(kalmanvert.getVelocity(),4,2,string2); //4:minimum width of output and 2 is number of decimals
   String string3 = ",P,";
   char string4[7];
-  dtostrf(ms5611_getPressure(),6,2,string4); //6:minimum width of output and 2 is number of decimals  
+  dtostrf(ms5611_getPressure(),6,2,string4); //6:minimum width of output and 2 is number of decimals
   String string5 = ",T,";
   char string6[6];
-  dtostrf(ms5611_getTemperature(),4,2,string6); //6:minimum width of output and 2 is number of decimals  
-  
-  
+  dtostrf(ms5611_getTemperature(),4,2,string6); //6:minimum width of output and 2 is number of decimals
+
+
   String OpenVario = string1 + string2 + string3 + string4 + string5 + string6;
-  
+
   int XOR = 0;
   int i = 0;
   // Calculate checksum ignoring any $'s in the string
@@ -293,7 +293,7 @@ void loop() {
 //}
 
 #endif //HAVE_BT
-    
+
 /**************//**************//**************//**************//**************//**************//**************//**************/
 #endif //HAVE_ACCELEROMETER
 
@@ -308,47 +308,26 @@ void loop() {
   /*****************/
 #ifdef HAVE_SPEAKER
   beeper.update();
-
-  /* check if near thermal features need to be started */
-#if defined(VARIOMETER_ENABLE_NEAR_CLIMBING_ALARM) || defined(VARIOMETER_ENABLE_NEAR_CLIMBING_BEEP)
-  if( !beepNearThermalEnabled ) {
-    /* check flight start conditions */
-    if( (millis() > FLIGHT_START_MIN_TIMESTAMP) &&
-        (kalmanvert.getVelocity() < FLIGHT_START_VARIO_LOW_THRESHOLD || kalmanvert.getVelocity() > FLIGHT_START_VARIO_HIGH_THRESHOLD)
-#ifdef HAVE_GPS
-        && gpsAltiCalibrated && (parser.getSpeed() > FLIGHT_START_MIN_SPEED)
-#endif //HAVE_GPS
-      ) {
-    beepNearThermalEnabled = true;
-#ifdef VARIOMETER_ENABLE_NEAR_CLIMBING_ALARM
-    beeper.setGlidingAlarmState(true);
-#endif
-#ifdef VARIOMETER_ENABLE_NEAR_CLIMBING_BEEP
-    beeper.setGlidingBeepState(true);
-#endif
-    }
-  }  
-#endif //defined(VARIOMETER_ENABLE_NEAR_CLIMBING_ALARM) || defined(VARIOMETER_ENABLE_NEAR_CLIMBING_BEEP)
 #endif //HAVE_SPEAKER
 
 
   /**************/
   /* update battery  */
   /**************/
-measuredvbat = analogRead(VBATPIN)*6.6/1024; 
+measuredvbat = analogRead(VBATPIN)*6.6/1024;
 //measuredvbat *= 2;    // we divided by 2, so multiply back
 //measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
 //measuredvbat /= 1024; // convert to voltage
 //Serial.print("VBat: " ); Serial.println(measuredvbat); /debug
-  
+
 
   /**************/
   /* update gps */
-  /**************/  
+  /**************/
 #ifdef HAVE_GPS
   if( Serial1.available() > 0 ) {
-  int inByte = Serial1.read();//debug
-    Serial.write(inByte);//debug
+//  int inByte = Serial1.read();//debug
+//    Serial.write(inByte);//debug
     /* if needed wait for GPS initialization */
     if( ! gpsDataStarted ) {
       while (Serial1.available() > 0) {
@@ -361,21 +340,22 @@ measuredvbat = analogRead(VBATPIN)*6.6/1024;
         }
       }
     }
-    
+
     /* else parse NMEA and save to sdcard */
     if( gpsDataStarted ) {
       while (Serial1.available() > 0) {
         int c = Serial1.read();
         parser.getChar(c);
-        Serial.write(c); //debug
+        //Serial.write(c); //debug
 
       }
     }
-  }
   
+
   /* recalibrate alti with gps */
   if( ! gpsAltiCalibrated ) {
    if( parser.haveNewAltiValue() ) {
+     Serial.println("1111");//debug
      gpsAltiCalibrationStep++;
      parser.getAlti(); //clear the new value flag
      if( gpsAltiCalibrationStep == GPS_CALIBRATION_STEPS) { //get 5 alti values before calibrating
@@ -383,11 +363,14 @@ measuredvbat = analogRead(VBATPIN)*6.6/1024;
        ms5611_setCurrentAltitude(gpsAlti);
        kalmanvert.resetPosition(gpsAlti);
        gpsAltiCalibrated = true;
+       Serial.print("ALTITUDE CALIBRATED BY GPS: ");//debug
+       Serial.println(gpsAlti);//debug
      }
    }
   }
+ }
 #endif //HAVE_GPS
-  
+
   /*****************/
   /* update screen */
   /*****************/
@@ -418,7 +401,7 @@ measuredvbat = analogRead(VBATPIN)*6.6/1024;
     }
     else {
       oled.setCursor(2,57);
-    }    
+    }
     oled.print(alt0,0);
 
     oled.setTextSize(3,2);
@@ -426,84 +409,42 @@ measuredvbat = analogRead(VBATPIN)*6.6/1024;
     oled.setCursor(5,0);
     oled.write(" "); //clear the minus sign (-) if velocity is positive
     oled.setCursor(5,17);
-    
+
     }
     else {
       oled.setCursor(5,0);
     }
-    
-    oled.print(kalmanvert.getVelocity(),1); 
+
+    oled.print(kalmanvert.getVelocity(),1);
 
     oled.setTextSize(1,1);
     oled.setCursor(7,99);
-    oled.print(measuredvbat); 
-    
-    NewDataEnable == 0; 
+    oled.print(measuredvbat);
+
+    NewDataEnable == 0;
   }
 
 
-  
+
 #ifdef HAVE_GPS
 
 
  if ( parser.haveNewAltiValue()) {              //added
-       Serial.print("GPSalti: ");//added
+       Serial.print("GPS ALTITUDE: ");//added
        Serial.println(parser.getAlti());//added
  }
 
-  /* when getting speed from gps, display speed and ratio */
+  /* when getting speed from gps, display speed */
   if ( parser.haveNewSpeedValue() ) {
+    Serial.print("GPSspeed: ");Serial.println(parser.getSpeed());//added
 
-    /* get new values */
-    unsigned long baseTime = speedFilterTimestamps[speedFilterPos];
-    unsigned long deltaTime = millis(); //computed later
-    speedFilterTimestamps[speedFilterPos] = deltaTime;
-    
-    double deltaAlti = speedFilterAltiValues[speedFilterPos]; //computed later
-    speedFilterAltiValues[speedFilterPos] = kalmanvert.getPosition(); 
-
-    double currentSpeed = parser.getSpeed();
-    speedFilterSpeedValues[speedFilterPos] = currentSpeed;
-
-    speedFilterPos++;
-    if( speedFilterPos >= VARIOMETER_SPEED_FILTER_SIZE )
-      speedFilterPos = 0;
-
-    /* compute deltas */
-    deltaAlti -= kalmanvert.getPosition();
-    deltaTime -= baseTime;
-    
-    /* compute mean distance */
-    double meanDistance = 0;
-    int step = 0;
-    while( step < VARIOMETER_SPEED_FILTER_SIZE ) {
-
-      /* compute distance */
-      unsigned long currentTime = speedFilterTimestamps[speedFilterPos];
-      meanDistance += speedFilterSpeedValues[speedFilterPos] * (double)(currentTime - baseTime);
-      baseTime = currentTime;
-
-      /* next */
-      speedFilterPos++;
-      if( speedFilterPos >= VARIOMETER_SPEED_FILTER_SIZE )
-        speedFilterPos = 0;
-      step++;
     }
 
-    /* compute glide ratio */
-    double ratio = (meanDistance/3600.0)/deltaAlti;
-
-    /* display speed and ratio */  
-    Serial.print("GPSspeed: ");Serial.println(parser.getSpeed());//added  
-//    //speedDigit.display( currentSpeed );    //TODO display speed on oled
-//    if( currentSpeed >= RATIO_MIN_SPEED && ratio >= 0.0 && ratio < RATIO_MAX_VALUE ) {
-//      ratioDigit.display(ratio);
-//    } else {
-//      ratioDigit.display(0.0);
-//    }
-  }
 #endif //HAVE_GPS
-#endif //HAVE_SCREEN 
+#endif //HAVE_SCREEN
+
+  }//end void loop
 
 
-}
+
+
